8091bd166ecf1d5d97779f6a7012248009416fb0
datlearner <kundaimuchemwa456@gmail.com>
2024-06-08 08:22:40 +0200

    sum function

--- app.js
+++ app.js
@@ -137,6 +137,18 @@ console.log('kundai');
 
 const fizzBuzz = () => {
     const result = [];
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i);
+        }
+    }
 }
 
 
