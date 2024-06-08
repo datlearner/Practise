fd079168936712166f3c40e1a28c312296635b9a
datlearner <kundaimuchemwa456@gmail.com>
2024-06-08 05:38:58 +0200

    simple function

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//Exercise 1: Simple Function
+//Write a function named greet that takes a single argument name and returns a greeting message.
+
+
+function simpleGreeting(name) {
+    return 'Welcome ' + name;
+};
+
+console.log(simpleGreeting('kundai'))
+
 const simpleHead = document.getElementById('simpleHeader');
 
 const lname = document.getElementById('lname');
@@ -5,5 +15,9 @@ const lname = document.getElementById('lname');
 const userDetails = document.getElementById('userDetails');
 
 userDetails.addEventListener('click', () => {
-    simpleHead.textContent =  `Welcome ${lname.value}`;
+    simpleHead.textContent =   `Welcome ${lname.value}`;
 });
+
+// Exercise 2: Sum of Two Numbers
+//Write a function named sum that takes two arguments a and b and returns their sum.
+
