7ac3ac807d3d68fb26869748a9f28d199037e41a
datlearner <kundaimuchemwa456@gmail.com>
2024-06-09 06:40:40 +0200

    objects

---
+++ objects/app.js
@@ -0,0 +1,18 @@
+//Exercise 1: Contact List Objective: Create a contact management system.
+/*
+Define a Contact Object:
+
+Each contact should have properties like name, phone number, and email. Implement Functions:
+
+Add a new contact to the list. Remove a contact by their name. List all contacts. Extended Features:
+
+Implement a search functionality to find contacts by name. Update the details of an existing contact.
+*/
+
+const Contact = {
+    name : 'Kundai',
+    phoneNumber : `065-456-8733`,
+    email : 'kundai@icloud.com'
+};
+
+console.log(Contact);
