03dfe2d8209b7ca11591e167c606e2e5572b11c8
datlearner <kundaimuchemwa456@gmail.com>
2024-07-20 11:40:52 +0200

    Created a class called person with properties name and age

--- classess/classes.js
+++ classess/classes.js
@@ -3,3 +3,15 @@
  * Define a class called Person with properties name and age.
  * Create an instance of the class and log the properties.
  */
+
+
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const firstPerson = new Person('kundai', 'muchemwa');
+
+console.log(firstPerson);
