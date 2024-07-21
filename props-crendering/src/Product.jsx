bf249abc239e933d21ce9356f375bf4fcb8e58eb
datlearner <kundaimuchemwa456@gmail.com>
2024-07-21 10:19:08 +0200

    starting up with react

--- props-crendering/src/Product.jsx
+++ props-crendering/src/Product.jsx
@@ -1,3 +1,20 @@
+/**
+ * Exercise 3: Product Component
+Objective: Create a Product component that displays product information based on props.
+
+Instructions:
+
+Create a Product component in a new file.
+The component should accept name, price, and inStock props.
+Display the product name and price.
+If inStock is true, display "In Stock".
+If inStock is false, display "Out of Stock".
+Use this component in your App component with different products.
+
+ */
+
+
+
 import PropTypes from 'prop-types'
 
 function Product (props) {
