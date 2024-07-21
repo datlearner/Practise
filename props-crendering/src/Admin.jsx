d284bd83a5f567282aa60c1dbc12b0331cd1a442
datlearner <kundaimuchemwa456@gmail.com>
2024-07-21 08:43:02 +0200

    starting up with react

--- props-crendering/src/Admin.jsx
+++ props-crendering/src/Admin.jsx
@@ -1,9 +1,10 @@
+
 function AdminDashboard(props){
     return(
         <div>
             <p>Name: {props.name}</p>
             <p>Age: {props.age} </p>
-            
+            <p>isStudent: {props.isStudent} </p>
         </div>
     );
 }
