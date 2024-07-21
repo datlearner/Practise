8e67a0669c197908a5a8cae67904b8147adac37f
datlearner <kundaimuchemwa456@gmail.com>
2024-07-21 17:30:55 +0200

    starting up with react

---
+++ props-crendering/src/Notification.jsx
@@ -0,0 +1,15 @@
+/**
+ * Exercise 4: Notification Component
+    Objective: Create a Notification component that shows different messages based on props.
+
+    Instructions:
+
+    Create a Notification component in a new file.
+    The component should accept a type prop which can be either "success" or "error".
+    If type is "success", display a success message.
+    If type is "error", display an error message.
+    Use this component in your App component to display both types of notifications.
+
+ */
+
+    
