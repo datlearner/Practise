1d30b68c8cbd7b172b4d9e76b67e4f1793ba42d0
datlearner <kundaimuchemwa456@gmail.com>
2024-07-21 09:27:33 +0200

    starting up with react

--- props-crendering/src/Greeting.jsx
+++ props-crendering/src/Greeting.jsx
@@ -15,7 +15,7 @@ import PropTypes from 'prop-types'
 function Greeting(props) {
     return(
         <div>
-            <h2> `Hello ${props.name}!` </h2>
+            <h2> Hello {props.name}! </h2>
         </div>
     )
 }
