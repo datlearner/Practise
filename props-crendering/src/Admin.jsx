f38b2f18ca6ec33278d2efac35fec68dd91d1596
datlearner <kundaimuchemwa456@gmail.com>
2024-07-21 09:06:53 +0200

    starting up with react

--- props-crendering/src/Admin.jsx
+++ props-crendering/src/Admin.jsx
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types'
 
 function AdminDashboard(props){
     return(
-        <div className="card">
+        <div className="flex">
+            <div className="card">
             <p>Name: {props.name}</p>
             <p>Age: {props.age} </p>
             <p>isStudent: {props.isStudent ? "Yes" : "No"}</p>
         </div>
+        </div>
+
     );
 }
 
