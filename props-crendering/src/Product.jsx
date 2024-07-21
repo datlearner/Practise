0f976c460c55c1ee17e5d4d32c8eda331d831cd3
datlearner <kundaimuchemwa456@gmail.com>
2024-07-21 10:24:54 +0200

    starting up with react

--- props-crendering/src/Product.jsx
+++ props-crendering/src/Product.jsx
@@ -20,7 +20,7 @@ function Product (props) {
         <div>
             <p>Name: {props.name} </p>
             <p>Price: {props.price} </p>
-            <p>inStock: {props.instock ? "Yes" : "No"}</p>
+            <p>inStock: {props.instock ? "In Stock" : "No"}</p>
         </div>
     );
 }
